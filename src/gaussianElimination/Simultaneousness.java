144ddb2fe87c53031f1b0ac1aaaff25604796121
ip-wataruabe <abe_wataru@interprism.cp.jp>
2017-07-28 00:17:18 +0900

    後退代入も実装

--- src/gaussianElimination/Simultaneousness.java
+++ src/gaussianElimination/Simultaneousness.java
@@ -32,13 +32,42 @@ public class Simultaneousness {
 		}
 	}
 
+	private double[] backwordSubstitution(){
+		double[] solution = new double[this.simultaneousEquation.length];
+		for(int i = this.simultaneousEquation.length - 1; i >= 0; i--){
+			double elementalSolution = this.simultaneousEquation[i].rightHandSide;
+
+			for(int j = this.simultaneousEquation.length - 1; j > i; j--){
+				elementalSolution -= this.simultaneousEquation[i].coefficients[j]*solution[j];
+			}
+
+			solution[i] = elementalSolution/this.simultaneousEquation[i].coefficients[i];
+		}
+
+		return solution;
+	}
+
 	void solve(){
 		forwardElimination();
+		System.out.println("The coefficient matrix is changed to ");
+		System.out.println();
 		for(int i = 0; i < this.simultaneousEquation.length; i++){
 			for(int j = 0; j < this.simultaneousEquation[i].coefficients.length; j++){
 				System.out.print(this.simultaneousEquation[i].coefficients[j] + " ");
 			}
 			System.out.println("| " + this.simultaneousEquation[i].rightHandSide);
 		}
+		System.out.println(".");
+
+		System.out.println("Also, the solution is");
+		double[] solution = backwordSubstitution();
+		String camma = "[ ";
+		for(double element:solution){
+			System.out.print(camma);
+			System.out.print(element);
+			camma = " , ";
+		}
+		System.out.println(" ].");
+		System.out.println("---------------------------------");
 	}
 }
--- src/gaussianElimination/SolvingController.java
+++ src/gaussianElimination/SolvingController.java
@@ -2,6 +2,7 @@ package gaussianElimination;
 
 public class SolvingController {
 	public static void main(String[] args){
+		//x-y=0,x+y=2
 		double[][] simultaneousEquationArray = {{1, -1, 0},{1, 1, 2}};
 
 		Simultaneousness simultaneousness =
@@ -10,5 +11,16 @@ public class SolvingController {
 						);
 
 		simultaneousness.solve();
+
+		//何も考えずに消去法で解くと計算機が計算ミスをする例･･･のはずだった。
+		//double型は浮動小数点型として優秀すぎて誤差が大きくなってくれない。4桁で計算すると誤差が大きくなるらしい。
+		double[][] stewart = {{0.001, 2, 3, 5.001},{-1, 3.712, 4.623, 7.335},{-2, 1.072, 5.643, 4.715}};
+
+		Simultaneousness stewartSimultaneousness =
+				new Simultaneousness(
+						Simultaneousness.twoDimensionalArrayAdapter(stewart)
+						);
+
+		stewartSimultaneousness.solve();
 	}
 }
